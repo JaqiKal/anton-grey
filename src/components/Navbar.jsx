93e41836a1168eefebbe94c7b690cd5909434494
JaqiKal <jaqika@gmail.com>
2024-12-31 12:52:40 +0100

    feat: Remove glassmorphism from navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
   }, [lastScrollY]);
 
   return (
-    <nav className={`fixed top-0 left-0 w-full bg-white bg-opacity-10 backdrop-blur-lg z-50 flex justify-between items-center px-8 py-4 transition-transform duration-300 ${showNavbar ? "translate-y-0" : "-translate-y-full"}`}>
+    <nav className={`fixed top-0 left-0 w-full  z-50 flex justify-between items-center px-8 py-4 transition-transform duration-300 ${showNavbar ? "translate-y-0" : "-translate-y-full"}`}>
       {/* Logo as a link to Home */}
       <Link
         to="/"
